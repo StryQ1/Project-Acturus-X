880364a67a5e1666f762d774c7ae792c47a3ca54
StryQ1 <96164824+StryQ1@users.noreply.github.com>
2022-03-21 16:53:42 +0200

    ActurusX py

--- src/ActurusX.py
+++ src/ActurusX.py
@@ -6,4 +6,7 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height=500, width=700, bg="black")
 canvas.pack()
 
+frame = tk.Frame(root, bg="#2b2b2b")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
 root.mainloop()
