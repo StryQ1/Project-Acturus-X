7ae2d608f03b2c1ce629b4c4948f89f55b73fbcd
StryQ1 <96164824+StryQ1@users.noreply.github.com>
2022-03-21 17:45:49 +0200

    ActurusX.py

--- src/ActurusX.py
+++ src/ActurusX.py
@@ -1,6 +1,8 @@
 
 import tkinter as tk
 
+mlbl = "Acturus X"
+
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=500, width=700, bg="black")
@@ -8,5 +10,8 @@ canvas.pack()
 
 frame = tk.Frame(root, bg="#2b2b2b")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+label = tk.Label(frame, text=mlbl, bg="#2b2b2b", fg="white")
+label.pack()
 
 root.mainloop()
