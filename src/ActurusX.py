5c8196ab68ec3441f4a3c1e7b5fa482b729d3362
StryQ1 <96164824+StryQ1@users.noreply.github.com>
2022-03-22 06:29:38 -0700

    Update ActurusX.py

--- src/ActurusX.py
+++ src/ActurusX.py
@@ -7,16 +7,6 @@ import tkinter as tk
 from tkinter.ttk import *
 from dark_title_bar import *
 
-def start():
-    load = 100
-    x = 0
-    speed = 1
-    while(x<task):
-        time.sleep(0.05)
-        bar ['value']+=(speed/load)*100
-        x+=speed
-        window.update_idletasks()
-
 # Values
 mlbl = "Acturus X"
 
@@ -28,8 +18,8 @@ root.title("Acturus X")
 root.iconbitmap("imgs/icon.ico")
 
 # Progressbar
-bar = Progressbar(root, orient=HORIZONTAL, lenght=350)
-bar.pack(pady=10)
+#bar = Progressbar(root, orient=HORIZONTAL, lenght=350)
+#bar.pack(pady=10)
 
 # Main app
 canvas = tk.Canvas(root, height=500, width=700, bg="black")
@@ -40,7 +30,7 @@ label = tk.Label(frame, text=mlbl, bg="#2b2b2b", fg="white")
 
 # Delay In splash screen
 time.sleep(5.5)
-bar.delete()
+#bar.delete()
 canvas.pack()
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 label.pack()
